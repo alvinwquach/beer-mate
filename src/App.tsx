0c70cab371a89d3b0cd9b42571a349cf9208e45a
Alvin Quach <alvinwquach@gmail.com>
2022-03-09 17:31:40 -0800

    added types

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { getFromApi, getBeers } from './beerapi'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TextField, Button } from '@mui/material';
+import { getFromApi, getBeers } from './beerapi'
 import './App.css';
 
 function App() {
+  const [beers, setBeers] = useState(getBeers);
+  console.log(getBeers);
+
   getFromApi().then((beers) => {
     console.log(beers[0].abv);
+    console.log(beers[10].name);
+    console.log(beers[0].tagline);
+    console.log(beers[0].ibu);
   });
+
+  
   return (
     <div className="App">
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
