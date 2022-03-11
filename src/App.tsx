c2bfdea4a6b1b7f1b0b177cff6fa17c48a1953b6
Alvin Quach <alvinwquach@gmail.com>
2022-03-11 14:29:58 -0800

    rendered list of beers

--- src/App.tsx
+++ src/App.tsx
@@ -19,14 +19,37 @@ function App() {
       // console.log(beers[0].tagline);
       // console.log(beers[0].ibu);
       setBeers(beers);
-      return;
     });
   }, [])
-  
+
   return (
     <div className="App">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Tagline</th>
+            <th>Description</th>
+            <th>ABV</th>
+            <th>Food Pairing</th>
+          </tr>
+        </thead>
+        <tbody>
+          {beers.map((beer) => {
+            return (
+              <tr key={beer.id}>
+                <td>{beer.name}</td>
+                <td>{beer.description}</td>
+                <td>{beer.tagline}</td>
+                <td>{beer.abv}</td>
+                <td>{beer.food_pairing}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <Button variant="contained">Submit</Button>
+      <Button variant="contained">Beer, Set, Match</Button>
     </div>
   );
 }
