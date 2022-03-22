e5b31975738cfb103853fc67c55e8671767ac5c8
Alvin Quach <alvinwquach@gmail.com>
2022-03-21 21:32:48 -0700

    added modal

---
+++ src/components/AlertDialog.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function AlertDialog() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Learn More!
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{""}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This beer was made with...
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            Thanks!
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
