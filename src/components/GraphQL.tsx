0fc61280687463a3e24fc0706c4efd58f2321f01
Alvin Quach <alvinwquach@gmail.com>
2022-03-29 22:54:30 -0700

    created graphql fragments

--- src/components/GraphQL.tsx
+++ src/components/GraphQL.tsx
@@ -1,67 +1,82 @@
 import { gql, useQuery } from "@apollo/client";
+const beerFragment = gql`
+  fragment beerFragment on Beer {
+    id
+    name
+    tagline
+    first_brewed
+    description
+    image_url
+    abv
+    ibu
+    target_fg
+    target_og
+    ebc
+    srm
+    ph
+    attenuation_level
+    volume {
+      value
+      unit
+    }
+    boil_volume {
+      value
+      unit
+    }
+    method {
+      mash_temp {
+        temp {
+          value
+          unit
+        }
+      }
+      fermentation {
+        temp {
+          value
+          unit
+        }
+      }
+      twist
+    }
+    ingredients {
+      malt {
+        name
+        amount {
+          value
+          unit
+        }
+      }
+      hops {
+        name
+        amount {
+          value
+          unit
+        }
+        add
+        attribute
+      }
+      yeast
+    }
+    food_pairing
+    brewers_tips
+    contributed_by
+  }
+`;
 
-const query = gql`
+const namequery = gql`
+  ${beerFragment}
   query GetBeerName($beername: String!) {
     getbeerbyname(name: $beername) {
-      id
-      name
-      tagline
-      first_brewed
-      description
-      image_url
-      abv
-      ibu
-      target_fg
-      target_og
-      ebc
-      srm
-      ph
-      attenuation_level
-      volume {
-        value
-        unit
-      }
-      boil_volume {
-        value
-        unit
-      }
-      method {
-        mash_temp {
-          temp {
-            value
-            unit
-          }
-        }
-        fermentation {
-          temp {
-            value
-            unit
-          }
-        }
-        twist
-      }
-      ingredients {
-        malt {
-          name
-          amount {
-            value
-            unit
-          }
-        }
-        hops {
-          name
-          amount {
-            value
-            unit
-          }
-          add
-          attribute
-        }
-        yeast
-      }
-      food_pairing
-      brewers_tips
-      contributed_by
+      ...beerFragment
+    }
+  }
+`;
+
+const allquery = gql`
+  ${beerFragment}
+  query GetAllBeers {
+    getbeer {
+      ...beerFragment
     }
   }
 `;
@@ -69,7 +84,7 @@ const query = gql`
 export function GraphQL() {
   // useQuery to query api data
   // add variables as an argument to useQuery
-  const { data } = useQuery(query, { variables: { beername: "Buzz" } });
+  const { data } = useQuery(namequery, { variables: { beername: "Buzz" } });
 
   return (
     <div className="Graphql">
