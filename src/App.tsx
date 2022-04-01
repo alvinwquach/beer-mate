50ea54b373cd4fae03942125105d1b8ab62e5eb0
Alvin Quach <alvinwquach@gmail.com>
2022-03-31 20:42:51 -0700

    styled buttons and text field

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { SchemaLink } from "@apollo/client/link/schema";
 import { schema } from "./graphql";
 
 import { TextField, Button } from "@mui/material";
+
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import "./App.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import BeerFetcher from "./components/BeerFetcher";
+
 type FormValues = {
   userInput: string;
 };
@@ -18,7 +20,6 @@ const client = new ApolloClient({
 });
 
 function App() {
-
   const [beerName, setBeerName] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
@@ -40,14 +41,6 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        {beerName ? <BeerFetcher beername={beerName} /> : null}
-        <div>
-          <h1>Welcome to Brewmate!</h1>
-          <p>
-            Type in your beer to find what to enjoy it with! Please note: Not
-            all beers may appear. Please click on the beer to learn more!
-          </p>
-        </div>
         <div
           style={{
             border: "1px solid red",
@@ -57,21 +50,48 @@ function App() {
         >
           <form onSubmit={handleSubmit(onSubmit)}>
             <TextField
+              style={{
+                display: "flex",
+                width: "100%",
+              }}
               id="outlined-basic"
               variant="outlined"
-              placeholder="Brewmate"
+              placeholder="Search for a beer"
               {...register("userInput")}
             />
+            {beerName ? <BeerFetcher beername={beerName} /> : null}
+            <div>
+              <h1>Welcome to Brewmate!</h1>
+              <p>
+                Type in your beer to find what to enjoy it with! Please note:
+                Not all beers may appear. Please click on the beer to learn
+                more!
+              </p>
+            </div>
             <div
               style={{
-                display: "grid",
-                gridArea: "revert",
+                border: "1px solid red",
               }}
             >
-              <Button variant="contained" type="submit">
+              <Button
+                style={{
+                  display: "inline-block",
+                  width: "50%",
+                }}
+                variant="contained"
+                type="submit"
+              >
                 Brew... Mate!
               </Button>
-              <Button variant="contained" color="error" onClick={handleClick}>
+              <Button
+                style={{
+                  display: "inline-block",
+                  width: "50%",
+                }}
+                variant="contained"
+                color="error"
+                onClick={handleClick}
+              >
                 Reset
               </Button>
             </div>
